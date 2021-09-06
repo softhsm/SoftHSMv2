6927b2d3227fcee176349ac4474faeef7d580b55
Jakub Jelen <jjelen@redhat.com>
2021-09-06 09:31:29 +0200

    Respect allowed mechanisms also in C_GetMechanismInfo
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/lib/SoftHSM.h
+++ src/lib/SoftHSM.h
@@ -510,7 +510,7 @@ private:
 	CK_RV MechParamCheckRSAPKCSOAEP(CK_MECHANISM_PTR pMechanism);
 	CK_RV MechParamCheckRSAAESKEYWRAP(CK_MECHANISM_PTR pMechanism);
 
-	bool isMechanismPermitted(OSObject* key, CK_MECHANISM_PTR pMechanism);
+	bool isMechanismPermitted(OSObject* key, CK_MECHANISM_TYPE mechanism);
 	void prepareSupportedMechanisms(std::map<std::string, CK_MECHANISM_TYPE> &t);
 	bool detectFork(void);
 };
