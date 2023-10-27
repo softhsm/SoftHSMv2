74553797bb8042c68a9c4ce9093da502507879ea
Neil Horman <nhorman@openssl.org>
2023-10-27 14:57:37 -0400

    Prevent accessing of global c++ objects once they are deleted
    Fixes (Maybe) #729.
    Reset objects_deleted after reset is called.

--- src/lib/SoftHSM.cpp
+++ src/lib/SoftHSM.cpp
@@ -87,6 +87,8 @@
 #include <unistd.h>
 #endif
 
+int objects_deleted = 0;
+
 // Initialise the one-and-only instance
 
 #ifdef HAVE_CXX11
@@ -406,6 +408,8 @@ void SoftHSM::reset()
 {
 	if (instance.get())
 		instance.reset();
+
+	objects_deleted = 0;
 }
 
 // Constructor
@@ -445,6 +449,7 @@ SoftHSM::~SoftHSM()
 
 	isInitialised = false;
 
+	objects_deleted = 1;
 	resetMutexFactoryCallbacks();
 }
 
