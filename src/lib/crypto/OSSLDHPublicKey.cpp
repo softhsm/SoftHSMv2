9687394c7c89efe0ff6e8d15f4559ecdc78079ef
Jakub Zelenka <bukka@php.net>
2026-02-25 16:15:02 +0100

    Migrate OSSLDH to OpenSSL 3.0+ (#817)

--- src/lib/crypto/OSSLDHPublicKey.cpp
+++ src/lib/crypto/OSSLDHPublicKey.cpp
@@ -36,10 +36,19 @@
 #include "OSSLDHPublicKey.h"
 #include "OSSLUtil.h"
 #include <openssl/bn.h>
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
 #ifdef WITH_FIPS
 #include <openssl/fips.h>
 #endif
+#else
+#include <openssl/core_names.h>
+#include <openssl/param_build.h>
+#include <openssl/provider.h>
+#endif
 #include <string.h>
+
+// The type
+/*static*/ const char* OSSLDHPublicKey::type = "OpenSSL DH Public Key";
 
 // Constructors
 OSSLDHPublicKey::OSSLDHPublicKey()
@@ -47,7 +56,19 @@ OSSLDHPublicKey::OSSLDHPublicKey()
 	dh = NULL;
 }
 
-OSSLDHPublicKey::OSSLDHPublicKey(const DH* inDH)
+// Check if the key is of the given type
+bool OSSLDHPublicKey::isOfType(const char* inType)
+{
+	return !strcmp(type, inType);
+}
+
+OSSLDHPublicKey::OSSLDHPublicKey(
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
+	const DH* inDH
+#else
+	const EVP_PKEY *inDH
+#endif
+)
 {
 	dh = NULL;
 
@@ -57,11 +78,40 @@ OSSLDHPublicKey::OSSLDHPublicKey(const DH* inDH)
 // Destructor
 OSSLDHPublicKey::~OSSLDHPublicKey()
 {
-	DH_free(dh);
+	resetOSSLKey();
 }
 
-// The type
-/*static*/ const char* OSSLDHPublicKey::type = "OpenSSL DH Public Key";
+
+// Setters for the DH public key components
+void OSSLDHPublicKey::setP(const ByteString& inP)
+{
+	DHPublicKey::setP(inP);
+
+	resetOSSLKey();
+}
+
+void OSSLDHPublicKey::setG(const ByteString& inG)
+{
+	DHPublicKey::setG(inG);
+
+	resetOSSLKey();
+}
+
+void OSSLDHPublicKey::setY(const ByteString& inY)
+{
+	DHPublicKey::setY(inY);
+
+	resetOSSLKey();
+}
+
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
+// Retrieve the OpenSSL representation of the key
+DH* OSSLDHPublicKey::getOSSLKey()
+{
+	if (dh == NULL) createOSSLKey();
+
+	return dh;
+}
 
 // Set from OpenSSL representation
 void OSSLDHPublicKey::setFromOSSL(const DH* inDH)
@@ -90,52 +140,13 @@ void OSSLDHPublicKey::setFromOSSL(const DH* inDH)
 	}
 }
 
-// Check if the key is of the given type
-bool OSSLDHPublicKey::isOfType(const char* inType)
+void OSSLDHPublicKey::resetOSSLKey()
 {
-	return !strcmp(type, inType);
-}
-
-// Setters for the DH public key components
-void OSSLDHPublicKey::setP(const ByteString& inP)
-{
-	DHPublicKey::setP(inP);
-
 	if (dh)
 	{
 		DH_free(dh);
 		dh = NULL;
 	}
-}
-
-void OSSLDHPublicKey::setG(const ByteString& inG)
-{
-	DHPublicKey::setG(inG);
-
-	if (dh)
-	{
-		DH_free(dh);
-		dh = NULL;
-	}
-}
-
-void OSSLDHPublicKey::setY(const ByteString& inY)
-{
-	DHPublicKey::setY(inY);
-
-	if (dh)
-	{
-		DH_free(dh);
-		dh = NULL;
-	}
-}
-
-// Retrieve the OpenSSL representation of the key
-DH* OSSLDHPublicKey::getOSSLKey()
-{
-	if (dh == NULL) createOSSLKey();
-
-	return dh;
 }
 
 // Create the OpenSSL representation of the key
@@ -173,3 +184,119 @@ void OSSLDHPublicKey::createOSSLKey()
 	DH_set0_pqg(dh, bn_p, NULL, bn_g);
 	DH_set0_key(dh, bn_pub_key, NULL);
 }
+#else
+
+// Retrieve the OpenSSL representation of the key
+EVP_PKEY* OSSLDHPublicKey::getOSSLKey()
+{
+	if (dh == NULL) createOSSLKey();
+	return dh;
+}
+
+// Set from OpenSSL representation
+void OSSLDHPublicKey::setFromOSSL(const EVP_PKEY* inDH)
+{
+	BIGNUM *bn_p = NULL, *bn_g = NULL, *bn_pub_key = NULL;
+	EVP_PKEY_get_bn_param(inDH, OSSL_PKEY_PARAM_FFC_P, &bn_p);
+	EVP_PKEY_get_bn_param(inDH, OSSL_PKEY_PARAM_FFC_G, &bn_g);
+	EVP_PKEY_get_bn_param(inDH, OSSL_PKEY_PARAM_PUB_KEY, &bn_pub_key);
+
+	if (bn_p)
+	{
+		ByteString inP = OSSL::bn2ByteString(bn_p);
+		setP(inP);
+		BN_free(bn_p);
+	}
+	if (bn_g)
+	{
+		ByteString inG = OSSL::bn2ByteString(bn_g);
+		setG(inG);
+		BN_free(bn_g);
+	}
+	if (bn_pub_key)
+	{
+		ByteString inY = OSSL::bn2ByteString(bn_pub_key);
+		setY(inY);
+		BN_free(bn_pub_key);
+	}
+}
+
+void OSSLDHPublicKey::resetOSSLKey()
+{
+	if (dh)
+	{
+		EVP_PKEY_free(dh);
+		dh = NULL;
+	}
+}
+
+// Create the OpenSSL representation of the key using OSSL_PARAM_BLD
+void OSSLDHPublicKey::createOSSLKey()
+{
+	if (dh != NULL) return;
+
+	BIGNUM* bn_p = OSSL::byteString2bn(p);
+	BIGNUM* bn_g = OSSL::byteString2bn(g);
+	BIGNUM* bn_pub_key = OSSL::byteString2bn(y);
+
+	EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_DH, NULL);
+	if (!ctx)
+	{
+		ERROR_MSG("Could not create EVP_PKEY_CTX object");
+		BN_free(bn_p);
+		BN_free(bn_g);
+		BN_free(bn_pub_key);
+		return;
+	}
+
+	OSSL_PARAM_BLD *bld = OSSL_PARAM_BLD_new();
+	if (!bld)
+	{
+		ERROR_MSG("Could not create OSSL_PARAM_BLD");
+		EVP_PKEY_CTX_free(ctx);
+		BN_free(bn_p);
+		BN_free(bn_g);
+		BN_free(bn_pub_key);
+		return;
+	}
+
+	if (!OSSL_PARAM_BLD_push_BN(bld, OSSL_PKEY_PARAM_FFC_P, bn_p) ||
+		!OSSL_PARAM_BLD_push_BN(bld, OSSL_PKEY_PARAM_FFC_G, bn_g) ||
+		!OSSL_PARAM_BLD_push_BN(bld, OSSL_PKEY_PARAM_PUB_KEY, bn_pub_key))
+	{
+		ERROR_MSG("Failed to push DH params to OSSL_PARAM_BLD");
+		OSSL_PARAM_BLD_free(bld);
+		EVP_PKEY_CTX_free(ctx);
+		BN_free(bn_p);
+		BN_free(bn_g);
+		BN_free(bn_pub_key);
+		return;
+	}
+
+	OSSL_PARAM *params = OSSL_PARAM_BLD_to_param(bld);
+	if (!params)
+	{
+		ERROR_MSG("OSSL_PARAM_BLD_to_param failed");
+		OSSL_PARAM_BLD_free(bld);
+		EVP_PKEY_CTX_free(ctx);
+		BN_free(bn_p);
+		BN_free(bn_g);
+		BN_free(bn_pub_key);
+		return;
+	}
+
+	if (EVP_PKEY_fromdata_init(ctx) <= 0 || EVP_PKEY_fromdata(ctx, &dh, EVP_PKEY_PUBLIC_KEY, params) <= 0)
+	{
+		ERROR_MSG("EVP_PKEY_fromdata failed");
+		dh = NULL;
+	}
+
+	OSSL_PARAM_free(params);
+	OSSL_PARAM_BLD_free(bld);
+	EVP_PKEY_CTX_free(ctx);
+	BN_free(bn_p);
+	BN_free(bn_g);
+	BN_free(bn_pub_key);
+}
+
+#endif
